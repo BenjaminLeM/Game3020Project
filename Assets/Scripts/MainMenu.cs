7d5d61b50ea34b115279f6f13db0f55e4b2b43fe
BenjaminLeM <Benjamin.LeMaistre@georgebrown.ca>
2024-12-03 07:28:47 -0500

    added functional start level selection to main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -24,43 +25,87 @@ public class MainMenu : MonoBehaviour
     Text InstructionText;
     [SerializeField]
     Text ControlsText;
+    [SerializeField]
+    RectTransform Levels;
+    [SerializeField]
+    List<Button> LevelButtons = new List<Button>();
     private void Awake()
     {
-        Button1.onClick.AddListener(delegate { StartGame(); });
+        Button1.onClick.AddListener(delegate { OpenLevelUI(); });
         Button2.onClick.AddListener(delegate { SwitchMenuToInstruction(); });
         Button3.onClick.AddListener(delegate { Quit(); });
         Button4.onClick.AddListener(delegate { SwitchMenuToMainMenu(); });
         Button5.onClick.AddListener(delegate { OpenInstructions(); });
         Button6.onClick.AddListener(delegate { OpenControls(); });
+        LevelButtons[0].onClick.AddListener(delegate { StartGame("FirstLevel"); });
+        LevelButtons[1].onClick.AddListener(delegate { StartGame("SecondLevel"); });
+        LevelButtons[2].onClick.AddListener(delegate { StartGame("ThirdLevel"); });
     }
 
-    void StartGame() 
+    void OpenLevelUI() 
+    {
+        SwitchMainButtonState();
+        List<LevelHighScore> levelData = new List<LevelHighScore>();
+        LevelHighScore data;
+        if (SaveLevelScore.tryLoadData(out data, "FirstLevel"))
+            levelData.Add(data);
+        if (SaveLevelScore.tryLoadData(out data, "SecondLevel"))
+            levelData.Add(data);
+        if (SaveLevelScore.tryLoadData(out data, "ThirdLevel"))
+            levelData.Add(data);
+        for (int i = 0; i < levelData.Count; i++) 
+        {
+            LevelButtons[i].transform.GetChild(1).GetComponent<TextMeshProUGUI>().text += levelData[i].HighScore.ToString();
+            LevelButtons[i].transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = "Grade: " + levelData[i].LevelGrade;
+        }
+
+        if (levelData.Count < 2) 
+        {
+            LevelButtons[2].transform.GetChild(0).gameObject.SetActive(true);
+            LevelButtons[2].GetComponent<Button>().enabled = false;
+            if (levelData.Count < 1) 
+            {
+                LevelButtons[1].transform.GetChild(0).gameObject.SetActive(true);
+                LevelButtons[1].GetComponent<Button>().enabled = false;
+            }
+        }
+        Levels.gameObject.SetActive(true);
+    }
+    void StartGame(string SceneName) 
     {
         Cursor.lockState = CursorLockMode.Locked;
         Time.timeScale = 1;
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene(SceneName);
     }
 
     #region
-
-    void SwitchActiveButtons() 
+    void SwitchMainButtonState() 
     {
         Button1.gameObject.SetActive(!Button1.gameObject.activeSelf);
         Button2.gameObject.SetActive(!Button2.gameObject.activeSelf);
         Button3.gameObject.SetActive(!Button3.gameObject.activeSelf);
+    }
+
+    void SwitchInstructionButtonState() 
+    {
         Button4.gameObject.SetActive(!Button4.gameObject.activeSelf);
         Button5.gameObject.SetActive(!Button5.gameObject.activeSelf);
         Button6.gameObject.SetActive(!Button6.gameObject.activeSelf);
     }
+    void SwitchMainmenuToInstructions() 
+    {
+        SwitchMainButtonState();
+        SwitchInstructionButtonState();
+    }
     void SwitchMenuToInstruction() 
     {
-        SwitchActiveButtons();
+        SwitchMainmenuToInstructions();
         TitleText.gameObject.SetActive(!TitleText.gameObject.activeSelf);
     }
 
     void SwitchMenuToMainMenu() 
     {
-        SwitchActiveButtons();
+        SwitchMainmenuToInstructions();
         TitleText.gameObject.SetActive(!TitleText.gameObject.activeSelf);
         if(InstructionText.gameObject.activeSelf)
             InstructionText.gameObject.SetActive(false);
